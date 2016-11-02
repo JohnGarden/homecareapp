e7df96acb8478c01a8681317bb254f60f8b88d84
JohnGarden <jrdmnt@gmail.com>
2016-11-02 17:28:17 -0200

    Add jOWL to read the ontology

---
+++ HomeCareApp/www/js/showtime.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  testFunction();
+  testFunction2();
+
+});
+
+function testFunction(){
+  var text = $("#testBizu").val();
+  
+  $("#testBizu2").html( text);
+}
+
+function testFunction2(){
+
+}
